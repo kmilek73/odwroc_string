b6669bc02988b0182dc889b85b17183a3d211ca4
kmilek <kmilek73@gmail.com>
2022-12-12 13:15:48 +0100

    poprawne rozwiązanie

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ function tekstToArray (tekst)
 const myArray=tekst.split('');
     console.log(myArray);
     let obroconaTablica =  myArray.reverse();
-    console.log(obroconaTablica)
+    console.log(obroconaTablica.join(" "));
+    console.log(typeof obroconaTablica.join());
+    console.log(typeof myArray);
 }
 
 tekstToArray(tekst);
